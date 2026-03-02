225f6dbae644aa27981b149d1940b463fb25bed4
dinhle03 <vandinh032004@gmail.com>
2026-03-02 18:03:10 +0700

    Bai Tap Buoi 5

--- bai2/src/main/java/com/example/bai2/model/Product.java
+++ bai2/src/main/java/com/example/bai2/model/Product.java
@@ -1,26 +1,25 @@
 package com.example.bai2.model;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotBlank(message = "Tên sản phẩm không được để trống")
     private String name;
 
-    @Length(min = 0, max = 200, message = "Tên hình ảnh không quá 200 kí tự")
+    @Length(max = 200, message = "Tên hình ảnh không quá 200 kí tự")
     private String image;
 
     @NotNull(message = "Giá sản phẩm không được để trống")
@@ -28,5 +27,7 @@ public class Product {
     @Max(value = 9999999, message = "Giá sản phẩm không được lớn hơn 9999999")
     private long price;
 
+    @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
 }
